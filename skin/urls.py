3bef673fea54cea846ae6d2b31e63ac64ada8439
Mandar Thakare <mandarthakare2@example.com>
2026-08-28 19:12:39 +0530

    Fix home page and prediction history URL

--- skin/urls.py
+++ skin/urls.py
@@ -1,42 +1,18 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
+    path("", views.home, name="home"),
 
-    # HOME
-    path(
-        "",
-        views.home,
-        name="home"
-    ),
+    path("register/", views.register_view, name="register"),
 
-    # REGISTER
-    path(
-        "register/",
-        views.register_view,
-        name="register"
-    ),
+    path("login/", views.login_view, name="login"),
 
-    # LOGIN
-    path(
-        "login/",
-        views.login_view,
-        name="login"
-    ),
+    path("logout/", views.logout_view, name="logout"),
 
-    # LOGOUT
-    path(
-        "logout/",
-        views.logout_view,
-        name="logout"
-    ),
-
-    # PREDICTION HISTORY
     path(
         "history/",
         views.prediction_history,
-        name="prediction_history"
+        name="history"
     ),
-
 ]
